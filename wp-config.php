1f308229c2d39327bd5b11c8c9454f26f28ba6f6
YoussefMhasni <79026302+YoussefMhasni@users.noreply.github.com>
2021-06-06 11:39:33 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,10 +20,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'freedbtech_mabase' );
+define( 'DB_NAME', 'kandy' );
 
 /** MySQL database username */
-define( 'DB_USER', 'freedbtech_youssef' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', 'mhasniCD33' );
